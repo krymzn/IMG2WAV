2d2e794a976fd67d61eb8241dacbff4a9f21fa0d
Rohit <rohit.dubey@softwaysolutions.com>
2022-07-29 14:13:43 +0530

    Fixed image upload, switching to new page fix needed

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require("cors")
 const path = require('path')
 const server = express()
 const getColors = require('get-image-colors')
-const cors = require("cors")
-const bodyParser = require('body-parser')
+const fs = require('fs')
 
 server.use(cors())
-server.use(bodyParser.json({ limit: '10mb' }));
-server.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+server.use(bodyParser.json({ limit: '15mb' }));
+// server.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }));
 
 
 const options = {
@@ -16,7 +17,7 @@ const options = {
 }
 
 let hslValuesArray = []
-let hexArray=[]
+let hexArray = []
 
 let notes = []
 let palette = [];
@@ -52,20 +53,49 @@ function getNotes(hslArr) {
     } else if ((330 <= hslArr[0] && hslArr[0] <= 359)) {
         notes.push("E5")
     }
-    
 }
 
-server.post("/upload-image", (req, res) => {
-    console.log("REQUEST BODY", req.body)
-    notes = []
-    palette=[]
-    let base64Data = req.body.file.replace(/^data:image\/(png|jpg|jpeg);base64,/, "");
+const deleteFileOnServer = () => {
+    return new Promise((resolve, reject) => {
+        if (fs.existsSync(path.join(__dirname, 'out.jpeg'))) {
+            fs.unlink(path.join(__dirname, 'out.jpeg'), (err) => {
+                if (err) {
+                    console.log(err)
+                    reject()
+                }
+                console.log(`${path.join(__dirname, 'out.jpeg')} was deleted successfully`);
+                resolve()
+            })
+        }
+    })
+}
 
-    require("fs").writeFile("out.jpeg", base64Data, 'base64', function (err) {
+deleteFileOnServer()
+
+
+server.post("/upload-image", (req, res) => {
+    console.log("UPLOAD IMAGE CALLED")
+    deleteFileOnServer()
+    try {
+        let base64Data = req.body.file.replace(/^data:image\/(png|jpg|jpeg);base64,/, "");
+        fs.writeFileSync(path.join(__dirname, "out.jpeg"), base64Data, {
+            encoding: "base64",
+            mode: 0o666
+        });
+        return JSON.stringify({ message: "File stored on the disk successfully" })
+    } catch (error) {
+        console.log(first)
+        return err
+    }
+
+})
+
+server.get("/getnotes", (req, res) => {
+    if (fs.existsSync(path.join(__dirname, "out.jpeg"))) {
         getColors(path.join(__dirname, 'out.jpeg'), options).then(colors => {
             hexArray = (colors.map((color) => color.hex()));
             for (let i = 0; i < hexArray.length; i++) {
-              getPalette(hexArray[i]);
+                getPalette(hexArray[i]);
             }
             hslValuesArray = colors.map(color => color.hsl())
             for (let i = 0; i < hslValuesArray.length; i++) {
@@ -73,18 +103,17 @@ server.post("/upload-image", (req, res) => {
             }
             res.json({ notes })
         })
-    });
-})
-
-server.get("/getnotes", (req, res) => {
-    res.json({ notes })
+    } else {
+        console.log("FILE DOES NOT EXIST ON THE SERVER")
+    }
 })
 
 server.get("/getPalette", (req, res) => {
-  res.json({ palette });
+    res.json({ palette });
+
 });
 
 
 
 
-server.listen("5001", () => { console.log(`Server running at http://localhost:5000`) })
+server.listen("5001", () => { console.log(`Server running at http://localhost:5001`) })
